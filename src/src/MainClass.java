368c2a6b97acd6ad38b5da46b13a61889a8272c0
Vadim Pasat <vadim.pasat@endava.com>
2020-10-26 18:59:38 +0200

    There are some changes. Line 4 and comment 5 is add

--- src/src/MainClass.java
+++ src/src/MainClass.java
@@ -1,5 +1,7 @@
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Hello Worl");
+        System.out.println("Commit and Push this changes");
+        //Add an comment below
     }
 }
