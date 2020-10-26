6ca3b085ddccfedf16f0eb09c2bd1dcaffafbd56
Vadim Pasat <vadim.pasat@endava.com>
2020-10-26 23:05:34 +0200

    Getter and Setter methods

--- src/src/MainClass.java
+++ src/src/MainClass.java
@@ -1,4 +1,23 @@
 public class MainClass {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello Worl");
         System.out.println("Commit and Push this changes");
