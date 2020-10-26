616fafdcb2c6fcd0284d1658e37f02a77f4a7437
Vadim Pasat <vadim.pasat@endava.com>
2020-10-26 22:20:45 +0200

    Array sum total

--- src/src/MainClass.java
+++ src/src/MainClass.java
@@ -3,5 +3,11 @@ public class MainClass {
         System.out.println("Hello Worl");
         System.out.println("Commit and Push this changes");
         //Add an comment below
+        int total = 0;
+        int [] arr = {5,1,7,2,7};
+        for (int i =0; i < arr.length; i++){
+           total += i;
+        }
+        System.out.println("Sum of array is: " + total);
     }
 }
