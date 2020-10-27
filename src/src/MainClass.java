ed6f53f0bc2cec734e0c009b69c31c7f1cc6e082
Vadim Pasat <vadim.pasat@endava.com>
2020-10-27 16:20:58 +0200

    Commit 1

--- src/src/MainClass.java
+++ src/src/MainClass.java
@@ -1,10 +1,6 @@
 public class MainClass {
    private String name;
    private int age;
-
-    String name;
-    int age;
-  
     public static void main(String[] args) {
         System.out.println("Hello Worl");
         System.out.println("Commit and Push this changes");
@@ -19,41 +15,5 @@ public class MainClass {
         }
         System.out.println("Sum of array is: " + total);
 
-        System.out.println(findIndex(arr,5));
-    }
- 
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-  
-    public static int findIndex(int arr[], int t)
-    {
-        if (arr == null) {
-            return -1;
-        }
-        int len = arr.length;
-        int i = 0;
-        while (i < len) {
-            if (arr[i] == t) {
-                return i;
-            }
-            else {
-                i = i + 1;
-            }
-        }
-        return -1;
     }
 }
