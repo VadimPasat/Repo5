6f031749d04d418204a8a540ec72ba9483888f46
Vadim Pasat <vadim.pasat@endava.com>
2020-10-27 16:22:49 +0200

    Commit 3

--- src/src/MainClass.java
+++ src/src/MainClass.java
@@ -1,9 +1,6 @@
 public class MainClass {
    private String name;
    private int age;
-
-//    String name;
-//    int age;
     public static void main(String[] args) {
         System.out.println("Hello Worl");
         System.out.println("Commit and Push this changes");
@@ -18,20 +15,5 @@ public class MainClass {
         }
         System.out.println("Sum of array is: " + total);
 
-    }
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 }
